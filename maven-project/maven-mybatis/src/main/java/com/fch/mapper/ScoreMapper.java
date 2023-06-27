20afee84f73015fa4e6099676e3c9ac49e2d0d53
FAN-CUNH <582389514@qq.com>
2023-06-27 18:33:12 +0800

    main:5

--- maven-project/maven-mybatis/src/main/java/com/fch/mapper/ScoreMapper.java
+++ maven-project/maven-mybatis/src/main/java/com/fch/mapper/ScoreMapper.java
@@ -1,13 +1,14 @@
 package com.fch.mapper;
 
 import com.fch.bean.Score;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 /**
  * @packageName com.fch.mapper
  * @ClassName ScoreMapper
- * @Description TODO
+ * @Description ScoreMapper
  * @Author Fan-CUNH
  * @Date 2023/6/27 11:45
  * @Version 1.0
@@ -19,4 +20,30 @@ public interface ScoreMapper {
      * @return 返回成绩对象集合包含学生信息
      */
     List<Score> selectAll();
+
+
+    /**
+     * 插入多个学生成绩
+     *
+     * @param scores 成绩集合
+     */
+    void insert(List<Score> scores);
+
+    /**
+     * 通过no批量删除成绩
+     *
+     * @param scoNos 成绩编号集合
+     */
+    void deleteByScoNos(List<String> scoNos);
+
+    /**
+     * 通过学生学号和课程编号查询学生成绩，可以动态查询
+     *      当两条件都为null时，查询全部成绩信息
+     *      当两个条件任意不为null时，按条件查询信息
+     *
+     * @param couNo 课程编号
+     * @param stuNo 学生学号
+     * @return 返回查询到的课程集合
+     */
+    List<Score> selectByCondition(@Param("couNo") String couNo,@Param("stuNo") String stuNo);
 }
--- maven-project/maven-mybatis/src/test/java/ScoreTest.java
+++ maven-project/maven-mybatis/src/test/java/ScoreTest.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -30,6 +31,65 @@ public class ScoreTest {
             List<Score> scores = scoreMapper.selectAll();
 
             //4. 输出结果
+            scores.forEach(System.out::println);
+        }
+    }
+
+    @Test
+    public void insert() {
+        //0. 获取SqlSessionFactory
+        SqlSessionFactory sqlSessionFactory = MybatisUtil.getSqlSessionFactory();
+
+        //1. 获取SqlSession
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            //2. 自动装载
+            ScoreMapper scoreMapper = session.getMapper(ScoreMapper.class);
+
+            //3. 调用 scoreMapper 的方法
+            List<Score> scores = Arrays.asList(
+                    new Score("006", "003", "001", 80, null),
+                    new Score("007", "003", "002", 80, null),
+                    new Score("008", "003", "003", 80, null));
+            scoreMapper.insert(scores);
+
+            //4. 提交结果
+            session.commit();
+        }
+    }
+
+    @Test
+    public void deleteByScoNos() {
+        //0. 获取SqlSessionFactory
+        SqlSessionFactory sqlSessionFactory = MybatisUtil.getSqlSessionFactory();
+
+        //1. 获取SqlSession
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            //2. 自动装载
+            ScoreMapper scoreMapper = session.getMapper(ScoreMapper.class);
+
+            //3. 调用 scoreMapper 的方法
+            List<String> scoNos = Arrays.asList("006", "007", "008");
+            scoreMapper.deleteByScoNos(scoNos);
+
+            //4. 提交结果
+            session.commit();
+        }
+    }
+
+    @Test
+    public void selectByCondition() {
+        //0. 获取SqlSessionFactory
+        SqlSessionFactory sqlSessionFactory = MybatisUtil.getSqlSessionFactory();
+
+        //1. 获取SqlSession
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            //2. 自动装载
+            ScoreMapper scoreMapper = session.getMapper(ScoreMapper.class);
+
+            //3. 调用 scoreMapper 的方法
+            List<Score> scores = scoreMapper.selectByCondition("001", "001");
+
+            //4. 输出结过
             scores.forEach(System.out::println);
         }
     }
